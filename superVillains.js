3de5ae7aca5318db837d26e2f7b4de9f4c0c0915
Arthur Gomes <arthurszaa7@icloud.com>
2025-03-15 17:51:13 -0300

    Subindo codigo

---
+++ superVillains.js
@@ -0,0 +1,5 @@
+import {randomSupervillain} from 'supervillains';
+
+export default function coolsolelog(mensagem) {
+    console.log("E em um momento de raiva",randomSupervillain(), "disse:", mensagem)
+}
